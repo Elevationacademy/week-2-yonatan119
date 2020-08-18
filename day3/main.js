add9185f641a6bd959ef725a01cc384209f1d5ee
yonatan.b <yonatanbenezra1@gmail.com>
2020-08-18 17:33:33 +0300

    day3ex

--- day3/main.js
+++ day3/main.js
@@ -207,3 +207,23 @@
 // bank.deposit(250)
 // bank.showBalance()
 //END
+
+//EX3
+// const SongsManager = () => {
+// let songs = {}
+// const addSong = (name,url) =>songs[name]= url.slice(32)
+// const getSong = (name) => {
+//     console.log("https://www.youtube.com/watch?v"+songs[name])}
+// return {
+//     addSong,
+//     getSong
+// }
+// }
+// const songsManager = SongsManager()
+// songsManager.addSong("sax", "https://www.youtube.com/watch?v=3JZ4pnNtyxQ")
+// songsManager.addSong("how long", "https://www.youtube.com/watch?v=CwfoyVa980U")
+// songsManager.addSong("ain't me", "https://www.youtube.com/watch?v=D5drYkLiLI8")
+
+// songsManager.getSong("sax")
+
+// END
